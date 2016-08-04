ee197b9826267a5cb74f5a63e62cb15aa99dce62
Zarema Khalilova <zarema.khalilova@gmail.com>
2016-08-05 01:39:42 +0300

    Remove unnecessary comments

--- package.js
+++ package.js
@@ -3,12 +3,8 @@ Npm.depends({'uploadcare-widget': '2.9.0'});
 Package.describe({
 	name: 'uploadcare:uploadcare-widget',
 	version: '1.0.0',
-	// Brief, one-line summary of the package.
 	summary: '',
-	// URL to the Git repository containing the source code for this package.
 	git: '',
-	// By default, Meteor will default to using README.md for documentation.
-	// To avoid submitting documentation, set this field to null.
 	documentation: 'README.md'
 });
 
--- uploadcare-widget-tests.js
+++ uploadcare-widget-tests.js
@@ -1,7 +1,4 @@
-// Import Tinytest from the tinytest Meteor package.
 import {Tinytest} from 'meteor/tinytest';
-
-// Import and rename a variable exported by uploadcare-widget.js.
 import uploadcare from 'meteor/uploadcare:uploadcare-widget';
 
 Tinytest.add('uploadcare-widget - uploadcare is object', function(test) {
